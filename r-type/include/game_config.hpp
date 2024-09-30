0aa50a657eb3ed54ce8df93cac5376b35c061230
Tom Blancheton <tom.blancheton@epitech.eu>
2024-09-30 18:44:14 +0200

    fix: bobber need space

--- r-type/include/game_config.hpp
+++ r-type/include/game_config.hpp
@@ -8,13 +8,6 @@
 #ifndef GAME_CONFIG_HPP
 #define GAME_CONFIG_HPP
 
-#define CONNECTION 0x01
-#define DISCONNECT 0x02
-#define MOVE_UP 0x03
-#define MOVE_DOWN 0x04
-#define MOVE_LEFT 0x05
-#define MOVE_RIGHT 0x06
-
 const int BUFFER_SIZE = 1024;
 const int PORT = 8080;
 const int MAX_PLAYERS = 4;
