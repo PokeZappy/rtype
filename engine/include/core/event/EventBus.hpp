c08d1447a582b5aa0d34833b9613c109ac196dfc
matthiasvonrakowski <matthias.von-rakowski@epitech.eu>
2024-10-02 12:41:44 +0200

    fixing : in a middle of a fix for the typeid

--- engine/include/core/event/EventBus.hpp
+++ engine/include/core/event/EventBus.hpp
@@ -23,30 +23,49 @@ namespace potEngine {
     
         template<class EventType>
         void publish(std::shared_ptr<EventType> event) {
-            std::shared_ptr<HandlerList> handlers = _subscribers[typeid(EventType)];
-            std::cout << "Event start to publish" << std::endl;
-            std::cout << "Event type " << typeid(EventType).name() << std::endl;
-
-            if (handlers == nullptr) {
-                std::cout << "No handler" << std::endl;
+            auto it = _subscribers.find(std::type_index(typeid(EventType)));
+            
+            if (it == _subscribers.end() || !it->second) {
+                std::cout << "No handler for event type: " << typeid(it->second).name() << std::endl;
                 return;
             }
-            for (auto& handler : *handlers)
+            // std::shared_ptr<HandlerList> handlers = _subscribers[typeid(EventType)];
+            // std::cout << "Event start to publish" << std::endl;
+            // std::cout << "Event type " << typeid(EventType).name() << std::endl;
+
+            // if (handlers == nullptr) {
+            //     std::cout << "No handler" << std::endl;
+            //     return;
+            // }
+            for (auto& handler : *it->second)
                 if (handler != nullptr)
-                    _handlers.push_back(std::make_pair(event, handlers));
-            std::cout << "Event published" << typeid(handlers).name() << std::endl;
+                    _handlers.push_back(std::make_pair(event, it->second));
+            std::cout << "Event published" << typeid(it->second).name() << std::endl;
         }
 
         template<class T, class EventType>
         void subscribe(T* instance, void (T::*memberFunction)(std::shared_ptr<EventType>)) {
-            std::shared_ptr<HandlerList> handlers = _subscribers[typeid(EventType)];
+            // auto it = _subscribers.find(typeid(EventType));
+            // std::shared_ptr<HandlerList> handlers = _subscribers[typeid(EventType)];
+    auto it = _subscribers.find(std::type_index(typeid(EventType)));
+    std::shared_ptr<HandlerList> handlers;
 
-            if (!handlers) {
-                handlers = std::make_shared<HandlerList>();
-                _subscribers[typeid(EventType)] = handlers;
-            }
-            handlers->push_back(std::make_shared<MemberHandler<T, EventType>>(instance, memberFunction));
-            std::cout << "Event subscribed "  << typeid(handlers).name() << _subscribers.size() << std::endl;
+    // If no entry exists for the event type, create a new handler list
+    if (it == _subscribers.end()) {
+        handlers = std::make_shared<HandlerList>();
+        _subscribers[std::type_index(typeid(EventType))] = handlers;
+    } else {
+        handlers = it->second;
+    }
+
+    // Add the new member handler for this event type
+    handlers->push_back(std::make_shared<MemberHandler<T, EventType>>(instance, memberFunction));
+        // if (!handlers) {
+        //         handlers = std::make_shared<HandlerList>();
+        //         _subscribers[typeid(EventType)] = handlers;
+        //     }
+        //     handlers->push_back(std::make_shared<MemberHandler<T, EventType>>(instance, memberFunction));
+            std::cout << "Event subscribed "  << typeid(handlers).name() << std::endl;
         }
 
         std::pair<std::shared_ptr<IEvent>, std::shared_ptr<HandlerList>> getHandler() {
