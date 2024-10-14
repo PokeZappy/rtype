d91fdd7a21bb6e287c242417d0efa566f7f2bbd5
Tom Blancheton <tom.blancheton@epitech.eu>
2024-10-15 00:30:35 +0200

    fix: remove useless line

--- engine/src/events/MainLoopEvent.cpp
+++ engine/src/events/MainLoopEvent.cpp
@@ -18,7 +18,6 @@ void potEngine::MainLoopEvent::eventMainLoop(std::shared_ptr<MainLoopEvent> even
 
     engine.timer.timerAddTick();
     engine.publishEvent(std::make_shared<NoneEvent>());
-    // std::cout << "main loop" << std::endl;
     engine.publishEvent(event);
 
     double tickDuration = 1.0f / engine.timer.timerGetTps();
@@ -28,12 +27,6 @@ void potEngine::MainLoopEvent::eventMainLoop(std::shared_ptr<MainLoopEvent> even
         double timeToSleep = tickDuration;
         std::this_thread::sleep_for(std::chrono::duration<double>(timeToSleep));
     }
-    // std::cout << "YOOOOO: " << std::chrono::high_resolution_clock::now() - now << std::endl;
-
-
-    std::chrono::duration<double> timeAfterSleep = engine.timer.getElapsedTimeSinceLastTick();
-
-    // std::cout << "TEMPS DE FOU: " << timeAfterSleep << std::endl;
     if (engine.timer.timerGetTick() >= engine.timer.timerGetTps()) {
         engine.timer.timerSetTick(0);
     }
